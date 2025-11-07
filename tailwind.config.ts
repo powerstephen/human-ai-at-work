2edf237c4478a728edc88b0c2e3f920751992dbb
powerstephen <power.stephen@gmail.com>
2025-11-07 17:37:21 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
         brand: {
-          blue: "#3366fe",
-          navy: "#0b1022"
-        }
-      }
-    }
+          DEFAULT: "#3366fe",
+          600: "#1f49f7",
+        },
+      },
+      borderRadius: {
+        "2xl": "1rem",
+      },
+    },
   },
   plugins: [],
 };
-
 export default config;
